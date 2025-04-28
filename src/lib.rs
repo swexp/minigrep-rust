ce316482f801a10461993789a067032cc55d2dc1
s <s@gemini>
2025-04-28 11:22:55 +0530

    update main and lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,7 @@
-use std::env;
 use std::fs;
-use std::process;
 use std::error::Error;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    dbg!(&args);
-
-    let config =Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing args: {err}");
-        process::exit(1);
-    });
-
-    println!("Searching for \"{}\" ", config.query);
-    println!("In file {}", config.file_path);
-
-    if let Err(e) = run(config) { 
-        println!("Application error : {e}");
-        process::exit(1);
-    };
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     
     let contents = fs::read_to_string(config.file_path)?;
 
@@ -30,14 +10,14 @@ fn run(config: Config) -> Result<(), Box<dyn Error>>{
     Ok(())
 }
 
-struct Config {
-    query: String,
-    file_path: String
+pub struct Config {
+    pub query: String,
+    pub file_path: String
 }
 
 impl Config {
 
-    fn build(args: &[String]) -> Result<Config, &'static str> {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
 
         if args.len() < 3 {
             return Err("Not enough arguments");
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,38 +15,8 @@ fn main() {
     println!("Searching for \"{}\" ", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(e) = run(config) { 
+    if let Err(e) = minigrep::run(config) { 
         println!("Application error : {e}");
         process::exit(1);
     };
 }
-
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("with text:\n {contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String
-}
-
-impl Config {
-
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }   
-}
-
