4a65be60b651c53e866e7436ee6315f4a10ed865
s <s@gemini>
2025-04-28 12:38:26 +0530

    add test

--- src/lib.rs
+++ src/lib.rs
@@ -30,3 +30,25 @@ impl Config {
     }   
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+
+
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+    //vec!["safe, fast, productive."]
+}
