2444ca611f90c81475757bb402d80ec258effd3b
s <s@gemini>
2025-04-28 14:36:08 +0530

    update run

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("with text:\n {contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+
+    }
 
     Ok(())
 }
