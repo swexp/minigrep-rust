3a6ef3218f52775df29279b6131dc88aa83df092
s <s@gemini>
2025-04-30 21:47:20 +0530

    improve using iterators

--- src/lib.rs
+++ src/lib.rs
@@ -29,14 +29,20 @@ use std::env;
 
 impl Config {
 
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
 
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not get a file_path string"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config { query, 
@@ -76,30 +82,20 @@ Trust me.";
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    //vec![]
-    //vec!["safe, fast, productive."]
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    //vec![]
-    //vec!["safe, fast, productive."]
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    let query_lowercase = query.to_lowercase();
+    contents
+        .lines()
+        .filter (|line| {
+            let line_lowercase = line.to_lowercase();
+            line_lowercase.contains(&query_lowercase)
+        })
+        .collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,8 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    dbg!(&args);
 
-    let config =Config::build(&args).unwrap_or_else(|err| {
+    let config =Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing args: {err}");
         process::exit(1);
     });
